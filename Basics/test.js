96186caffdb10b9978c3af000c655d206aaf1267
Bhavesh Rajpurohit <rajpurohitbhavesh2027@gmail.com>
2025-06-10 18:37:23 +0000

    Configure File Done

---
+++ Basics/test.js
@@ -0,0 +1,3 @@
+//Today, we have started our journey of Refining our Javascript Knowledge right from the scratch again.
+//We have learnt to print things, learnt to display things.
+console.log("Bhavesh Rajpurohit")
