c4347599dfa2e9af0aae24958ab83fdf4985c5d1
Bhavesh Rajpurohit <rajpurohitbhavesh2027@gmail.com>
2025-06-22 12:33:47 +0000

    All about Strings

---
+++ Basics/Strings.js
@@ -0,0 +1,16 @@
+//***********STRINGS and It's METHODS***********************/
+//A Kind of Datatype where there is a group of English Alphabet placed between either of Double quotes or single quotes.
+
+
+const Name = "Bhavesh Rajpurohit"
+let Age = 21
+
+//Concept :- STRING INTERPOLATION :- It means merging 2 or more strings or memory location's value together combinely.
+
+/*We Don't use this anymore cuz it's so odd method :- 
+console.log(Name+Age)*/
+
+//Instead we use this :- 
+console.log(`${Name} is of ${Age} and he is a Blockchain Developer.`)
+
+//Methods of Strings :- 
