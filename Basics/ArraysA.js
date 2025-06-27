6b64461e8efcfce810551e60cb1c31caf65bbee5
Bhavesh Rajpurohit <rajpurohitbhavesh2027@gmail.com>
2025-06-27 04:41:29 +0000

    Basic Arrays Part - 1

---
+++ Basics/ArraysA.js
@@ -0,0 +1,15 @@
+//******************************ARRAYS********************//
+/*Arrays are the group of dynamically heterogeneous Data type where you can store as much as data or value to that memory location as per your choice*/ 
+
+//You can declare and initialize arrays by two methods :- 
+const Fruits = ["Banana","Papaya", "Grapes"]
+const Games = new Array ("GTA V", "RDR2", "GTA VI") 
+
+console.log(Fruits)
+console.log(Games)
+console.log(typeof (Fruits, Games))
+
+//There are a lot of Methods in Arrays with the help of which you can perform any task.
+
+
+
